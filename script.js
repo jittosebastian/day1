a11c231f5248d2a68b4b0aaa3bc0e30f8f65ab13
jittosebastian <jittosebastian718@gmail.com>
2024-07-24 15:47:18 +0530

    First  Day

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+// const persons ={
+
+//     firstname: "Kevin",
+
+//     lastname:"doe",
+
+//     age:22,
+
+//     eyecolor:'blue'
+
+// }
+
+// console.log(persons.firstname +" "+ "is"+" " +persons.age+"old")
+
+// for (let 1=0; i<=10; i++){
+
+//console.log("looping through+1");
+
+//}
+
+//function square(num)
+
+//{
+
+// return num*num;
+
+//}
+
+//let sum=(num1,num2)=>{
+
+//  return num1+num2;
+
+//}
+//console.log(sum(44,30));
+
+
+
